4c956497239db7468ab81ae9a0ab6e47b8e4278a
dddsuz2 <daidai121207@gmail.com>
2023-08-05 00:26:01 +0900

    add: 記事

--- scripts/gen_link.py
+++ scripts/gen_link.py
@@ -1,10 +1,20 @@
 import os
+import re
+from natsort import natsorted
 
 directory = input()
 
 files = os.listdir(directory)
 
-files = sorted(files)
+def extract_number(s):
+    # 正規表現で数値を抽出
+    match = re.search(r'【([\d\.]+)】', s)
+    if match:
+        return [float(t) if t.replace('.', '', 1).isdigit() else float('inf') for t in match.group(1).split('.')]
+    else:
+        return [9999]
+
+files = sorted(files, key=extract_number)
 
 for file_name in files:
     name, extension = os.path.splitext(file_name)
